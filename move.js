1d301582213488e974ee021d6d8818130898dc37
ConnellyR <connellyrogers@gmail.com>
2018-09-19 12:46:37 -0700

    added style centered and js

--- move.js
+++ move.js
@@ -2,17 +2,13 @@ $(document).ready(function(){
 		
 		$("#surf").click(function(){
 		$("#surf").animate({left:'+=100px'},'slow', )
-		
-		
-		
-		
-		
-		//$("#p9").click(function(){
-		//$("#force").animate({left:'+=100px'},'slow', )
-		//$("#force").animate({left:'-=100px'},'slow', )
-		//});
-		
-		//$("#p14").click(function(){
-		//$("#pull").animate({left:'+=400px'},'slow', )
 		});
+		
+		$("#sail").click(function(){
+		$("#sail").animate({top:'-=100px'},'slow', )
+		
+		});
+		
+		
+		
 	})
