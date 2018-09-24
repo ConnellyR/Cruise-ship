1118217b6b0e1bda00194f0cb39475ef7bc90256
benjaminelizalde24 <benjaminelizalde24@gmail.com>
2018-09-23 20:16:50 -0700

    added locations and connected everything else

---
+++ pics.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".locations").hide();
+    });
+    $("#show").click(function(){
+        $(".locations").show();
+    });
+});
+
