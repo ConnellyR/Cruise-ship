61e50b7919896eaec35ce55baeaa4067e95b5d4f
ConnellyR <42652898+ConnellyR@users.noreply.github.com>
2018-10-09 10:16:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,17 +6,34 @@ app = Flask(__name__)
 def render_main():
     return render_template('home.html')
 
-@app.route("/first")
+@app.route("/Map")
 def render_page1():
-    return render_template('page1.html')
+    return render_template('Map.html')
 
-@app.route("/p2")
+@app.route("/activities")
 def render_page2():
-    return render_template('page2.html')
+    return render_template('activities.html')
 
-@app.route("/p3")
+@app.route("/Ticket-and-Prices")
 def render_page3():
-    return render_template('page 3.html')
+    return render_template('Ticket-and-Prices.html')
+
+@app.route("/store")
+def render_page4():
+    return render_template('store.html')
+
+@app.route("/Resturant1")
+def render_page5():
+    return render_template('Resturant1.html')
+
+@app.route("/Resturant2")
+def render_page6():
+    return render_template('Resturant2.html')
+
+@app.route("/locations")
+def render_page7():
+    return render_template('locations.html')
+
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
